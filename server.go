9be6d3d970ae93bdd3d0ac19939b182aec8192a0
ujstor <astipan@gmail.com>
2023-09-03 21:20:55 +0200

    go server bug

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Handle requests for static assets (CSS, fonts, images, and JavaScript)
+	fs := http.FileServer(http.Dir("web"))
+	http.Handle("/web/", http.StripPrefix("/web/", fs))
+
+	// Handle requests for the root path ("/")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/index.html")
+	})
+
+	port := 8080
+	fmt.Printf("Server listening on port %d...\n", port)
+	addr := fmt.Sprintf(":%d", port)
+	http.ListenAndServe(addr, nil)
+}
